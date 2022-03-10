c5a97b97370c470b22c285f1a7f14e0b8e02b3bd
Ginakalu <gdkalu@gmail.com>
2022-03-10 08:05:27 -0800

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * Description: print text using puts
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle\n");
-	Return (0);
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
